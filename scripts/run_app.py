8ce233260d3357d449266227357ff3e24cdfa411
Ostap Baranov <wolfboomm7@mail.ru>
2025-04-20 21:48:27 +0300

    build(script): add new dir with launcher
    so it's saving the relative imports with all tests and fix binaries at the same time

---
+++ scripts/run_app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from src import main
+
+if __name__ == "__main__":
+    main.main()  # Для сохранения относительных импортов
